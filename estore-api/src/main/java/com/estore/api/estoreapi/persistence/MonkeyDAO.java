80bb0ecb094dea953a039fb91088512bb0007236
awb8593 <awb8593@rit.edu>
2022-02-07 19:18:16 -0500

    MonkeyDAO
    Started writing MonkeyDAO.java and MonkeyFileDAO.java

--- estore-api/src/main/java/com/estore/api/estoreapi/persistence/MonkeyDAO.java
+++ estore-api/src/main/java/com/estore/api/estoreapi/persistence/MonkeyDAO.java
@@ -1,5 +1,26 @@
 package com.estore.api.estoreapi.persistence;
 
-public class MonkeyDAO {
+import java.io.IOException;
+import com.estore.api.estoreapi.model.Monkey;
+
+/**
+ * Defines the interface for Monkey object persistence
+ * 
+ * @author Adrian Burgos awb8593
+ */
+public interface MonkeyDAO {
+    
+    /**
+     * Creates and saves a {@linkplain Monkey monkey}
+     * 
+     * @param monkey {@linkplain Monkey monkey} object to be created and saved
+     * <br>
+     * The id of the monkey object is ignored and a new uniqe id is assigned
+     *
+     * @return new {@link Monkey monkey} if successful, false otherwise 
+     * 
+     * @throws IOException if an issue with underlying storage
+     */
+    Monkey createProduct(Monkey monkey) throws IOException;
     
 }
--- estore-api/src/main/java/com/estore/api/estoreapi/persistence/MonkeyFileDAO.java
+++ estore-api/src/main/java/com/estore/api/estoreapi/persistence/MonkeyFileDAO.java
@@ -1,5 +1,13 @@
 package com.estore.api.estoreapi.persistence;
 
+/**
+ * Implements the functionality for JSON file-based peristance for Monkeys
+ * 
+ * {@literal @}Component Spring annotation instantiates a single instance of this
+ * class and injects the instance into other classes as needed
+ * 
+ * @author Adrian Burgos awb8593
+ */
 public class MonkeyFileDAO {
     
 }
