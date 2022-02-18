d0a69f2e1da258b1e2335688591bcac9c119c4fd
awb8593 <awb8593@rit.edu>
2022-02-17 19:19:53 -0500

    product-page component created
    generated the component for product-page using the angular CLI

--- estore-ui/angular-monkey-estore/src/app/app.module.ts
+++ estore-ui/angular-monkey-estore/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ProductPageComponent } from './product-page/product-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductPageComponent
   ],
   imports: [
     BrowserModule
