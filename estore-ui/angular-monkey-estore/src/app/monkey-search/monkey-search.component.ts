d49e48ce863bbaaec3bd7e6f8f40afafe0704c1f
jrh3397 <jrh3397@rit.edu>
2022-02-21 18:04:53 -0500

    Added Search Bar
    Service has not yet been implemented on this branch

--- estore-ui/angular-monkey-estore/src/app/monkey-search/monkey-search.component.ts
+++ estore-ui/angular-monkey-estore/src/app/monkey-search/monkey-search.component.ts
@@ -1,4 +1,16 @@
+/**
+ * Author: Jack Hester
+ * Note: Must check any comment "!!!"
+ */
+
 import { Component, OnInit } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+import {
+    debounceTime, distinctUntilChanged, switchMap
+  } from 'rxjs/operators'
+
+import { Monkey } from '../monkey'; // !!!
+import { MonkeyService } from '../monkey.service'; // !!!
 
 @Component({
   selector: 'app-monkey-search',
@@ -7,9 +19,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MonkeySearchComponent implements OnInit {
 
-  constructor() { }
+  monkeys$!: Observable<Monkey[]>;
+  private searchTerms = new Subject<string>();
+
+  constructor(private monkeyService: MonkeyService) { } // !!!
+
+  // Push a search term into the observable stream.
+  search(term: string): void {
+    this.searchTerms.next(term);
+  }
 
   ngOnInit(): void {
+    this.monkeys$ = this.searchTerms.pipe( // !!!
+      // wait 300 ms after each keystroke before considering the term
+      debounceTime(300),
+
+      // ignore new term if same as previous teerm
+      distinctUntilChanged(),
+
+      // switch to new search observable each time the term changes
+      switchMap((term: string) =>
+      this.monkeyService.searchMonkeys(term)),
+    );
   }
 
 }
