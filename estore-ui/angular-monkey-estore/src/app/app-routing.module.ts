049ff1ec0ba1f403d825791b1bb673bec486f735
taw8452 <taw8452@rit.edu>
2022-02-21 14:35:35 -0500

    started BuyerProductList
    Added BuyerProductList, monkey service, monkey interface, and routing

---
+++ estore-ui/angular-monkey-estore/src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { BuyerProductListComponent } from './buyer-product-list/buyer-product-list.component';
+
+const routes : Routes = [
+  { path: 'buyerproductlist', component: BuyerProductListComponent }
+];
+
+@NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule { }
--- estore-ui/angular-monkey-estore/src/app/app.module.ts
+++ estore-ui/angular-monkey-estore/src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BuyerProductListComponent } from './buyer-product-list/buyer-product-list.component';
+import { MonkeysComponent } from './monkeys/monkeys.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BuyerProductListComponent,
+    MonkeysComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
