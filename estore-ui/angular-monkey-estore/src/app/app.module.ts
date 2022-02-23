94beadb44dfc2de0a530b8bb96879db3ffec47ac
edr5068 <edr5068@rit.edu>
2022-02-22 19:41:01 -0500

    made it so that monkey names can now be edited from owner page

--- estore-ui/angular-monkey-estore/src/app/app.module.ts
+++ estore-ui/angular-monkey-estore/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { OwnerPageComponent } from './owner-page/owner-page.component';
@@ -14,7 +15,8 @@ import { AppRoutingModule } from './app-routing.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
