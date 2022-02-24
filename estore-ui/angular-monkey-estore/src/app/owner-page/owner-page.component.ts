374a7c1dd531eb2b3ae09b3eec36760fef66a8db
edr5068 <edr5068@rit.edu>
2022-02-23 20:21:02 -0500

    Made it so that owner features can add, delete, and update monkeys in the system

--- estore-ui/angular-monkey-estore/src/app/owner-page/owner-page.component.ts
+++ estore-ui/angular-monkey-estore/src/app/owner-page/owner-page.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Monkey } from '../monkey';
 import { MONKEYS } from './mock-monkey';
+import { MonkeyService } from '../monkey.service';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-owner-page',
@@ -8,16 +10,50 @@ import { MONKEYS } from './mock-monkey';
   styleUrls: ['./owner-page.component.css']
 })
 export class OwnerPageComponent implements OnInit {
-
-  monkeys = MONKEYS;
-  selectedMonkey?: Monkey;
-
-  constructor() { }
-
+  
   ngOnInit(): void {
+    this.getMonkeys();
   }
+
+  constructor(
+    private monkeyService: MonkeyService,
+    private location: Location
+  ) { }
 
   onSelect(monkey: Monkey): void {
     this.selectedMonkey = monkey;
+  }
+
+  monkey: Monkey | undefined;
+
+  monkeys: Monkey[] = [];
+  selectedMonkey?: Monkey;
+
+  getMonkeys(): void {
+    this.monkeyService.getMonkeys()
+    .subscribe(monkeys => this.monkeys = monkeys);
+  }
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.monkeyService.addMonkey({ name } as Monkey)
+      .subscribe(monkey => {
+        this.monkeys.push(monkey);
+      });
+  }
+
+  delete(monkey: Monkey): void {
+    this.monkeys = this.monkeys.filter(m => m !== monkey);
+    this.monkeyService.deleteMonkey(monkey.id).subscribe();
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+
+  update(monkey: Monkey): void {
+    this.monkeyService.updateMonkey(monkey).subscribe(monkey => {this.monkeys.push(monkey);
+    });
   }
 }
