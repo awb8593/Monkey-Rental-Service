ff179b325d9972999b5ee23c35e561c04e1960fc
taw8452 <taw8452@rit.edu>
2022-03-02 18:29:53 -0500

    Added button clicking
    Add function do detect which monkey was selected and I added more primates to the json file

--- estore-ui/angular-monkey-estore/src/app/buyer-product-list/buyer-product-list.component.ts
+++ estore-ui/angular-monkey-estore/src/app/buyer-product-list/buyer-product-list.component.ts
@@ -10,6 +10,7 @@ import { MonkeyService } from '../monkey.service';
 })
 export class BuyerProductListComponent implements OnInit {
   monkeys: Monkey[] = [];
+  message:string = "";
 
   constructor(private monkeyService: MonkeyService) { }
 
@@ -20,5 +21,9 @@ export class BuyerProductListComponent implements OnInit {
   getMonkeys(): void {
     this.monkeyService.getMonkeys()
     .subscribe(monkeys => this.monkeys = monkeys);
+  }
+
+  clickEvent(monkey: Monkey): void {
+    this.message = monkey.name + " has been clicked ";
   }
 }
