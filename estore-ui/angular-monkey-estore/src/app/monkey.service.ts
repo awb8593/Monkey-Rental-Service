dc339d01de33d80489499c6ef9b782fd94a0651f
taw8452 <taw8452@rit.edu>
2022-03-13 20:54:25 -0400

    fixed some stuff
    Fixed some merge errors and added buyer product list to app component html

--- estore-ui/angular-monkey-estore/src/app/monkey.service.ts
+++ estore-ui/angular-monkey-estore/src/app/monkey.service.ts
@@ -84,16 +84,4 @@ export class MonkeyService {
     );
 
   }
-
-  private handleError<T>(operation = 'operation', result?: T){
-    return (error: any): Observable<T> => {
-      console.error(error);
-      this.log(`${operation} failed: ${error.message}`);
-      return of(result as T);
-    };
-  }
-
-  private log(message: string){
-  //  this.messageService.add(`MonkeyService: ${message}`);
-  }
 }
