dba9ebdc903160ba40df26f9bbd70cbc53baf8e1
edr5068 <edr5068@rit.edu>
2022-03-13 20:15:55 -0400

    Finalized owner features

--- estore-ui/angular-monkey-estore/src/app/app-routing.module.ts
+++ estore-ui/angular-monkey-estore/src/app/app-routing.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { OwnerPageComponent } from './owner-page/owner-page.component';
 import { BuyerProductListComponent } from './buyer-product-list/buyer-product-list.component';
 
 const routes: Routes = [
-  { path: 'owner-page', component: OwnerPageComponent},
   { path: 'buyer-product-list', component: BuyerProductListComponent }
 ];
 
