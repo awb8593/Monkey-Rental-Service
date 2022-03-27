245204c651205bd54228b4ecfeaec1154474adbb
awb8593 <awb8593@rit.edu>
2022-03-27 17:06:09 -0400

    Created ReviewFileDAO
    created ReviewFileDAO.java to implement ReviewDAO's methods

---
+++ estore-api/src/main/java/com/estore/api/estoreapi/persistence/ReviewFileDAO.java
@@ -0,0 +1,16 @@
+package com.estore.api.estoreapi.persistence;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Implements the functionality for JSON file-based peristance for Reviews
+ * 
+ * {@literal @}Component Spring annotation instantiates a single instance of this
+ * class and injects the instance into other classes as needed
+ * 
+ * @author Adrian Burgos awb8593
+ */
+@Component
+public class ReviewFileDAO implements ReviewDAO{
+    
+}
