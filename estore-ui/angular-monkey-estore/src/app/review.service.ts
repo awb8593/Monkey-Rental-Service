7372a862edb77fde47626b9e132cc0defbca7107
taw8452 <taw8452@rit.edu>
2022-03-30 10:55:07 -0400

    Made Review interface and service

---
+++ estore-ui/angular-monkey-estore/src/app/review.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
+
+import { Review } from './review';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ReviewService {
+
+  private reviewsUrl = 'http://localhost:8080/reviews';
+
+  httpOptions = {
+    headers: new HttpHeaders({'Content-Type': 'application/json'})
+  };
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getReviews(id: number): Observable<Review[]> {
+    return this.http.get<Review[]>(this.reviewsUrl);
+  }
+}
---
+++ estore-ui/angular-monkey-estore/src/app/review.ts
@@ -0,0 +1,5 @@
+export interface Review {
+    id: number;
+    ratings: number[];
+    reviews: String[];
+}
