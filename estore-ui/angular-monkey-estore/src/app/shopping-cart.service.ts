967ddc67a1426009cce88fb1b8691727aceeafc9
awb8593 <awb8593@rit.edu>
2022-03-30 18:04:30 -0400

    Updated clearCart()
    updated clearCart method to add each item as a rental

--- estore-ui/angular-monkey-estore/src/app/shopping-cart.service.ts
+++ estore-ui/angular-monkey-estore/src/app/shopping-cart.service.ts
@@ -5,6 +5,9 @@ import { MonkeyService } from './monkey.service';
 
 import { CurrentUserService } from './current-user.service';
 
+import { RentalService } from './rental.service';
+import { Rental } from './rental';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -14,7 +17,8 @@ export class ShoppingCartService {
   
   constructor(
     private currentUserService: CurrentUserService,
-    private monkeyService: MonkeyService
+    private monkeyService: MonkeyService,
+    private rentalService: RentalService
     ) {}
 
   addToCart(monkey: Monkey){
@@ -32,6 +36,20 @@ export class ShoppingCartService {
   }
 
   clearCart(){
+    for(let i = 0; i < this.items.length; i ++) { // add each item to the user's rentals
+      let monkeyid = this.items[i].id
+      let tempDate = new Date();
+      tempDate.setDate(tempDate.getDate() + 7);
+      var rental = {
+        id: 0,
+        rentalDate: new Date().toJSON(),
+        returnDate: tempDate.toJSON(),
+        userId: this.currentUserService.user.id,
+        monkeyId: monkeyid,
+        active: true
+      }
+      this.rentalService.addRental(rental).subscribe();
+    }
     this.items = [];
     this.currentUserService.user.cartList = [];
     this.currentUserService.save();
